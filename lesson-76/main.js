4031898225219191e0dad5a4829edef215a6d540
SvyatoeVoskresenie <156691649+SvyatoeVoskresenie@users.noreply.github.com>
2024-09-18 02:31:44 +0300

    Add files via upload

---
+++ lesson-76/main.js
@@ -0,0 +1,12 @@
+const firstName = 'Anastasia'
+const lastName = 'Kononova'
+const age = '21'
+let city = 'Nizhny Novgorod'
+let job = 'poor student and screenwriter'
+
+console.log (firstName)
+console.log (lastName)
+console.log (age, 'y.o')
+console.log (city, '||| city where i live')
+console.log (job, '||| this is who am i:(')
+
