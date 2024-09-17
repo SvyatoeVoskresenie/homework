37ced1ce8bbdc040fa61b5ae48e3aa050c70d891
SvyatoeVoskresenie <156691649+SvyatoeVoskresenie@users.noreply.github.com>
2024-09-18 02:48:25 +0300

    add template

---
+++ lesson-77/main.js
@@ -0,0 +1,14 @@
+const firstName = 'Anastasia '
+const lastName = 'Kononova'
+
+const x = 8
+let y = 1 
+const num1 = 7656987698
+const num2 = 9876543
+const num3 = 9
+
+console.log (firstName + lastName)
+console.log (y - x)
+console.log (-num3)
+console.log (num1 / num2 * x) 
+
