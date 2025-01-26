62e2067a3cf19c8662be4a94b5743565bafdbea1
SvyatoeVoskresenie <156691649+SvyatoeVoskresenie@users.noreply.github.com>
2025-01-26 20:07:26 +0300

    add template

---
+++ lesson-95/main.js
@@ -0,0 +1,25 @@
+const firstName = 'Anastasia '
+const lastName = 'Kononova'
+
+// const x = 8
+// let y = 1 
+// const num1 = 7656987698
+// const num2 = 9876543
+// const num3 = 9
+
+// console.log (firstName + lastName)
+// console.log (y - x)
+// console.log (-num3)
+// console.log (num1 / num2 * x) 
+
+// console.log('3421764' + '345678')
+
+const x = 5
+const y = 2
+let z = 1 
+const num1 = 1456789
+const num2 = -1
+const num3 = 11
+
+console.log('1) x + y + z =', x + y + z)
+console.log('2)', -num1, num2 + z / x, x ** y )
