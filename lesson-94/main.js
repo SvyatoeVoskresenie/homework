bcf7b68821e801460c8e6d206a5907b7edbca425
SvyatoeVoskresenie <156691649+SvyatoeVoskresenie@users.noreply.github.com>
2025-01-26 20:06:29 +0300

    add template

---
+++ lesson-94/main.js
@@ -0,0 +1,8 @@
+const firstName = 'Anastasia'
+const secondName = 'Kononova'
+let age = '21'
+const gender = 'female'
+
+console.log('|||', firstName, secondName, '|||', age, 'y.o |||', gender, '|||')
+
+
