d94c2c9fbc672a1cb4d9d067e84709352202a084
SvyatoeVoskresenie <156691649+SvyatoeVoskresenie@users.noreply.github.com>
2025-01-26 20:08:05 +0300

    add template

---
+++ lesson-96/main.js
@@ -0,0 +1,10 @@
+const myAge = 21
+const minimumAge = 18
+
+if(myAge !== minimumAge){
+    console.log('You are free to go')
+} else{
+    console.log('Go to bed')
+}
+
+myAge !== minimumAge ? console.log('You may visit this such of web') : console.log('Pls watch the Disney channel')
