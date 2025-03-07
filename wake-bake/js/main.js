b2f16da52d4347021eacb1e06735669884b6f793
SvyatoeVoskresenie <156691649+SvyatoeVoskresenie@users.noreply.github.com>
2025-03-07 08:06:02 +0300

    add template

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,9 +1,32 @@
+// Modal
 
-// Хэдер
+const modalIsOpening = document.querySelector('.about__img-button')
+const modalIsOpened = document.querySelector('.body')
+const modalIsClosed = document.querySelector('.modal__cancel')
 
-(function () {
+modalIsOpening.addEventListener('click', () => {
+    modalIsOpened.classList.add('body--opened-modal')
+}
+)
 
-    document.addEventListener('click', burgerIsCooked) 
+modalIsClosed.addEventListener('click', () => {
+    modalIsOpened.classList.remove('body--opened-modal')
+}
+)
+// modalIsOpening.addEventListener('click', () => {
+//     modalIsOpened.classList.add('body--opened-modal')
+// }
+// )
+// modalIsClosed.addEventListener('click', () => {
+//     modalIsOpened.classList.remove('body--opened-modal')
+// }
+// )
+
+    // Хэдер
+
+    (function () {
+
+        document.addEventListener('click', burgerIsCooked)
 
         function burgerIsCooked(e) {
             const target = e.target
@@ -11,17 +34,17 @@
             const burgerIsHere = target.closest('.burger-icon')
             const headerNavLink = target.closest('.header__nav-link')
 
-            if(document.documentElement.clientWidth > 900) return
+            if (document.documentElement.clientWidth > 900) return
 
-            if(burgerIsHere){
+            if (burgerIsHere) {
                 navMenu.classList.toggle('body--menu-is-opened')
             }
-            if(headerNavLink){
+            if (headerNavLink) {
                 navMenu.classList.toggle('body--menu-is-opened')
             }
 
         }
 
 
-})()
+    })()
 
