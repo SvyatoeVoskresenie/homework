ac14254bc4e07429134e546e185a7a6ba2d91774
SvyatoeVoskresenie <156691649+SvyatoeVoskresenie@users.noreply.github.com>
2025-03-11 05:57:36 +0300

    add template

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -110,6 +110,45 @@
     //         }
     //     })
     // })
+
+
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// slider-gallery||||||||||||||||||||||||||||||||||||||||
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+const swiper = new Swiper('.gallery__swiper', {
+    spaceBetween: 15,
+    slidesPerView: 1.5,
+
+    pagination: {
+        el: '.gallery__pagination',
+        type: 'fraction',
+    },
+
+    navigation: {
+        nextEl: '.gallery__next',
+        prevEl: '.gallery__prev',
+    },
+
+    breakpoints: {
+        601: {
+            slidesPerView: 3,
+        },
+        801: {
+            spaceBetween: 32,
+        },
+        1101: {
+            slidesPerView: 4,
+        }
+    }
+
+    });
+
+
+
+
+
+
 }) ()
 
 
