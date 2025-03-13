69b3503b34f8fbb1cec366c29ca17dc17e5a5b5b
SvyatoeVoskresenie <156691649+SvyatoeVoskresenie@users.noreply.github.com>
2025-03-13 20:02:06 +0300

    add template

---
+++ сalifornia/california/js/main.js
@@ -0,0 +1,31 @@
+
+// ||||||||||||||||||||||
+
+(function () {
+
+
+        new Swiper('.hero__swiper', {
+        spaceBetween: 40,
+        slidesPerView: 1,
+        // centeredSlides: true,
+    
+        navigation: {
+            nextEl: '.hero__next',
+            prevEl: '.hero__prev',
+        },
+
+        
+        breakpoints: {
+            901: {
+                slidesPerView: 1.5,
+            },
+            1201: {
+                slidesPerView: 2.1,
+            }
+        }
+
+        
+    });
+    
+}) ()
+
