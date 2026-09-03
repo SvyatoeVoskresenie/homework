e9468adabfa9957bddd90aa55167e759865e1ae6
SvyatoeVoskresenie <156691649+SvyatoeVoskresenie@users.noreply.github.com>
2026-09-03 18:43:22 +0300

    add template

--- js-lesson-67/js/main.js
+++ js-lesson-67/js/main.js
@@ -3,41 +3,79 @@
 const todoKeys = {
   id: "id",
   text: "text",
-  isCompleted: "is_completed"
+  isCompleted: "is_completed",
 };
 const todos = [];
 const errTodoNotFound = id => `Todo with id ${id} not found`;
 
-const getNewTodosId = array =>
-  array.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+const getNewTodosId = todos =>
+  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
 
-const createTodo = (array, text) => {
+const createTodo = (todos, text) => {
   const newTodo = {
     [todoKeys.id]: getNewTodosId(todos),
     [todoKeys.text]: text,
     [todoKeys.isCompleted]: false,
   };
-  array.push(newTodo);
-  return array;
+  todos.push(newTodo);
+  return todos;
 };
 
-const changeTodo = (array, id, text) => {
-  let todo = array.find(todo => todo[todoKeys.id] === id);
+const changeTodoStatus = (todos, id) => {
+  let todo = todos.find(todo => todo[todoKeys.id] === id);
   if (!todo) {
     console.error(errTodoNotFound(id));
     return null;
   }
   todo[todoKeys.isCompleted] = !todo[todoKeys.isCompleted];
-  todo[todoKeys.text] = text
   return todo;
 };
 
-const deleteTodo = (array, id) => {
-  const todoIndex = array.findIndex(todo => todo[todoKeys.id] === id);
+const changeTodoText = (todos, id, text) => {
+  let todo = todos.find(todo => todo[todoKeys.id] === id);
+  if (!todo) {
+    console.error(errTodoNotFound(id));
+    return null;
+  }
+  todo[todoKeys.text] = text;
+  return todo;
+};
+
+const deleteTodo = (todos, id) => {
+  const todoIndex = todos.findIndex(todo => todo[todoKeys.id] === id);
   if (todoIndex === -1) {
     console.error(errTodoNotFound(id));
-    return array;
+    return todos;
   }
-  array.splice(todoIndex, 1);
-  return array;
+  todos.splice(todoIndex, 1);
+  return todos;
 };
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
+
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector(".input");
+const todosEl = document.querySelector(".todos");
+
+let numCounter = 2;
+const count = () => numCounter++
+
+const createTodoElement = () => {
+  const todoEl = document.createElement("li");
+  todoEl.classList.add("todo");
+  todoEl.innerHTML = `
+          <div class="todo-text">Задача ${count()}</div>
+          <div class="todo-actions">
+            <button class="button-complete button">&#10004;</button>
+            <button class="button-delete button">&#10006;</button>
+          </div>
+    `;
+  todosEl.append(todoEl);
+};
+
+const handleCreateTodo = (todos, text) => {
+  createTodo(todos, text)
+  createTodoElement()
+}
